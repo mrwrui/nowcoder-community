ba63a73938025ce5ba6aae4c6efcad02a0d649a7
mrwrui <1445673608@qq.com>
2024-01-19 15:33:14 +0800

    开发了验证码刷新的功能

---
+++ src/main/java/com/nowcoder/community/community/entity/LoginTicket.java
@@ -0,0 +1,12 @@
+package com.nowcoder.community.community.entity;
+
+import java.util.Date;
+
+public class LoginTicket {
+    private int id;
+    private int userId;
+    private String ticket;
+    private int status;
+    private Date expired;
+
+}
