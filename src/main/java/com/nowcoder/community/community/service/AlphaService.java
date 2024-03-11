dc6a5d76fb4d06d02a9b760e0b3b4e10adaf6703
mrwrui <1445673608@qq.com>
2024-03-11 14:27:32 +0800

    事务管理

--- src/main/java/com/nowcoder/community/community/service/AlphaService.java
+++ src/main/java/com/nowcoder/community/community/service/AlphaService.java
@@ -1,10 +1,24 @@
 package com.nowcoder.community.community.service;
 
 import com.nowcoder.community.community.dao.AlphaDao;
+import com.nowcoder.community.community.dao.DiscussPostMapper;
+import com.nowcoder.community.community.dao.UserMapper;
+import com.nowcoder.community.community.entity.DiscussPost;
+import com.nowcoder.community.community.entity.User;
+import com.nowcoder.community.community.util.CommunityUtil;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.util.Date;
 
 @Service
 //@Scope("prototype")
@@ -12,6 +26,15 @@ public class AlphaService {
 
     @Autowired
     private AlphaDao alphaDao;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
 
     public AlphaService() {
 //        System.out.println("实例化AlphaService");
@@ -29,6 +52,70 @@ public class AlphaService {
 
     public String find() {
         return alphaDao.select();
+    }
+
+    // 杜撰一个业务，新建一个user并且发一个帖子，认为这两个动作是一个事务
+    // 事务传播机制是解决两个业务交叉的问题，业务A调用了业务B
+    // REQUIRED：支持当前事务（A,外部事务），如果不存在则创建新事务。
+    // REQUIRES_NEW：创建一个新的事务并且暂停当前事务（A外部事务）
+    // NESTED：如果当前存在事务（A外部事务）则事务B嵌套在当前事务A中执行（独立的提交和回滚），否则就会和REQUIRED一样
+    // 声明式事务
+    @Transactional(isolation = Isolation.READ_UNCOMMITTED,propagation = Propagation.REQUIRED)
+    public Object save1() {
+        // 新增用户
+        User user = new User();
+        user.setUsername("Alpha");
+        user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+        user.setPassword(CommunityUtil.md5("123" + user.getSalt()));
+        user.setEmail("Alpha@qq.com");
+        user.setHeaderUrl("http://image.nowcoder.com/head/99t.png");
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user);
+
+        //新增帖子
+        DiscussPost post = new DiscussPost();
+        post.setUserId(user.getId());
+        post.setTitle("hello");
+        post.setContent("新人报到");
+        post.setCreateTime(new Date());
+        discussPostMapper.insertDiscussPost(post);
+
+        // 故意写一个错误，看会不会回滚
+        Integer.valueOf("abc");
+
+        return "ok";
+    }
+
+    // 编程式事务
+    public Object save2() {
+        transactionTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
+        transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        return transactionTemplate.execute(new TransactionCallback<Object>() {
+            @Override
+            public Object doInTransaction(TransactionStatus status) {
+                // 新增用户
+                User user = new User();
+                user.setUsername("beta");
+                user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+                user.setPassword(CommunityUtil.md5("123" + user.getSalt()));
+                user.setEmail("beta@qq.com");
+                user.setHeaderUrl("http://image.nowcoder.com/head/999t.png");
+                user.setCreateTime(new Date());
+                userMapper.insertUser(user);
+
+                //新增帖子
+                DiscussPost post = new DiscussPost();
+                post.setUserId(user.getId());
+                post.setTitle("你好");
+                post.setContent("我是新人");
+                post.setCreateTime(new Date());
+                discussPostMapper.insertDiscussPost(post);
+
+                // 故意写一个错误，看会不会回滚
+                Integer.valueOf("abc");
+                return "ok";
+            }
+        });
     }
 
 }
---
+++ src/test/java/com/nowcoder/community/community/TransactionTests.java
@@ -0,0 +1,32 @@
+package com.nowcoder.community.community;
+
+
+import com.nowcoder.community.community.service.AlphaService;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class TransactionTests {
+    @Autowired
+    private AlphaService alphaService;
+
+    @Test
+    public void testSave1() {
+        Object obj = alphaService.save1();
+        System.out.println(obj);
+    }
+
+    @Test
+    public void testSave2() {
+        Object obj = alphaService.save2();
+        System.out.println(obj);
+    }
+}
