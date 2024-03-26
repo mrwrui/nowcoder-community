b6716bd6f63c1b1e14d7a7044e9a62ab9f229349
mrwrui <1445673608@qq.com>
2024-03-26 23:26:43 +0800

    配置Redis已经点赞功能

--- src/main/java/com/nowcoder/community/community/controller/DiscussPostController.java
+++ src/main/java/com/nowcoder/community/community/controller/DiscussPostController.java
@@ -6,6 +6,7 @@ import com.nowcoder.community.community.entity.Page;
 import com.nowcoder.community.community.entity.User;
 import com.nowcoder.community.community.service.CommentService;
 import com.nowcoder.community.community.service.DiscussPostService;
+import com.nowcoder.community.community.service.LikeService;
 import com.nowcoder.community.community.service.UserService;
 import com.nowcoder.community.community.util.CommunityConstant;
 import com.nowcoder.community.community.util.CommunityUtil;
@@ -32,6 +33,9 @@ public class DiscussPostController implements CommunityConstant {
 
     @Autowired
     private CommentService commentService;
+
+    @Autowired
+    private LikeService likeService;
 
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     @ResponseBody
@@ -61,6 +65,14 @@ public class DiscussPostController implements CommunityConstant {
         User user = userService.findUserById(post.getUserId());
         model.addAttribute("user",user);
 
+        // 赞的数量
+        long likeCount = likeService.findEntityLikeCount(ENTITY_TYPE_POST,post.getId());
+        model.addAttribute("likeCount",likeCount);
+
+        // 点赞状态
+        int likeStatus = hostHolder.getUser()==null? 0 : likeService.findEntityLikeStatus(hostHolder.getUser().getId(),ENTITY_TYPE_POST,post.getId());
+        model.addAttribute("likeStatus",likeStatus);
+
         // 评论的分页信息
         page.setLimit(5);
         page.setPath("/discuss/detail/" + discussPostId);
@@ -82,6 +94,14 @@ public class DiscussPostController implements CommunityConstant {
                 // 作者
                 commentVo.put("user", userService.findUserById(comment.getUserId()));
 
+                // 赞的数量
+                likeCount = likeService.findEntityLikeCount(ENTITY_TYPE_COMMENT,comment.getId());
+                commentVo.put("likeCount",likeCount);
+
+                // 点赞状态
+                likeStatus = hostHolder.getUser()==null ? 0 : likeService.findEntityLikeStatus(hostHolder.getUser().getId(),ENTITY_TYPE_COMMENT,comment.getId());
+                commentVo.put("likeStatus",likeStatus);
+
                 // 回复列表
                 List<Comment> replyList = commentService.findCommentByEntity(ENTITY_TYPE_COMMENT,comment.getId(),0,Integer.MAX_VALUE);
                 // 回复VO列表
@@ -96,6 +116,15 @@ public class DiscussPostController implements CommunityConstant {
                         // 回复目标
                         User target = reply.getTargetId()==0 ? null: userService.findUserById(reply.getTargetId());
                         replyVo.put("target",target);
+
+                        // 赞的数量
+                        likeCount = likeService.findEntityLikeCount(ENTITY_TYPE_COMMENT,reply.getId());
+                        replyVo.put("likeCount",likeCount);
+
+                        // 点赞状态
+                        likeStatus = hostHolder.getUser()==null ? 0 : likeService.findEntityLikeStatus(hostHolder.getUser().getId(),ENTITY_TYPE_COMMENT,reply.getId());
+                        replyVo.put("likeStatus",likeStatus);
+
                         replyVoList.add(replyVo);
                     }
                 }
---
+++ src/main/java/com/nowcoder/community/community/controller/LikeController.java
@@ -0,0 +1,39 @@
+package com.nowcoder.community.community.controller;
+
+import com.nowcoder.community.community.entity.User;
+import com.nowcoder.community.community.service.LikeService;
+import com.nowcoder.community.community.util.CommunityUtil;
+import com.nowcoder.community.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class LikeController {
+
+    @Autowired
+    private LikeService likeService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @PostMapping("/like")
+    @ResponseBody
+    public String like(int entityType, Integer entityId) {
+        User user = hostHolder.getUser();
+        // 点赞
+        likeService.like(user.getId(), entityType,entityId);
+        // 数量
+        Long likeCount = likeService.findEntityLikeCount(entityType,entityId);
+        // 状态
+        int status = likeService.findEntityLikeStatus(user.getId(), entityType,entityId);
+        Map<String, Object> map = new HashMap<>();
+        map.put("likeCount", likeCount);
+        map.put("likeStatus", status);
+        return CommunityUtil.getJSONString(0, null, map);
+    }
+}
